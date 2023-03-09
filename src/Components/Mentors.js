48c57b90632f1df344cc18c3d90f297f642c8e7f
lakshmanK1 <laxmankamisetty1@gmail.com>
2023-03-09 21:37:45 +0530

    created the circular div for iamges

--- src/Components/Mentors.js
+++ src/Components/Mentors.js
@@ -1,12 +1,18 @@
 import React from 'react'
-import { MainDiv, MentorsDiv, CircleDiv, ContentDiv, P } from './StyledCmp'
+import { MainDiv, MentorsDiv, CircleDiv, ContentDiv, SpinnerDiv, RoundDiv} from './StyledCmp'
 
 function Mentors() {
   return (
     <MainDiv>
         <MentorsDiv>
             <CircleDiv>
-        
+                <SpinnerDiv>
+                    <RoundDiv>1</RoundDiv>
+                    <RoundDiv>2</RoundDiv>
+                    <RoundDiv>3</RoundDiv>
+                    <RoundDiv>4</RoundDiv>
+                    <RoundDiv>5</RoundDiv>
+                </SpinnerDiv>
             </CircleDiv>
         </MentorsDiv>
     </MainDiv>
--- src/Components/StyledCmp.js
+++ src/Components/StyledCmp.js
@@ -1,4 +1,11 @@
 import styled from 'styled-components'
+import {keyframes} from 'styled-components'
+
+export const rotate = keyframes`
+    100%{
+        transform:rotate(1turn) translate(100px) rotate(-1turn)
+    }
+`;
 
 export const MainDiv = styled.div`padding:40px;`;
 
@@ -17,9 +24,23 @@ align-items:center; overflow:hidden;
     margin-left:215px;
 }`;
 
+
 export const CircleDiv = styled.div`position:relative; z-index:1;
 margin:0 auto; max-width:900px; text-align:center;`;
 
 export const ContentDiv = styled.div``;
 
 export const P = styled.p`margin:0; padding:0; color:#fff; font-size:2em;`;
+
+export const SpinnerDiv = styled.div`--n:5; --d:12s; width:200px; height:200px; margin:40px auto; border:1px solid #000;
+display:grid; grid-template-columns:30px; grid-template-rows:30px; place-content:center; 
+border-radius:50%;`;
+
+export const RoundDiv = styled.div`grid-area:1/1; line-height:30px; text-align: center; border-radius:50%; background:#f00; 
+animation: ${rotate} var(--d) linear infinite; transform: rotate(0) translate(100px) rotate(0);
+:nth-child(1) {animation-delay:calc(-0*var(--d)/var(--n))}
+:nth-child(2) {animation-delay:calc(-1*var(--d)/var(--n))}
+:nth-child(3) {animation-delay:calc(-2*var(--d)/var(--n))}
+:nth-child(4) {animation-delay:calc(-3*var(--d)/var(--n))}
+:nth-child(5) {animation-delay:calc(-4*var(--d)/var(--n))}`;
+
