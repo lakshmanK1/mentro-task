756572920c3670b7b75fd851e73ea3cdd9e8a0b4
lakshmanK1 <laxmankamisetty1@gmail.com>
2023-03-10 16:41:41 +0530

    done some changes  and created separate profile displayer

--- src/Components/Mentors.js
+++ src/Components/Mentors.js
@@ -1,22 +1,55 @@
-import React from 'react'
-import { MainDiv, MentorsDiv, CircleDiv, ContentDiv, SpinnerDiv, RoundDiv} from './StyledCmp'
+import React,{useState} from 'react'
+
+import {BsChevronLeft, BsChevronRight, BsFillStarFill} from 'react-icons/bs'
+import { ImageUrls } from './StyledCmp'
+import { MentorData } from './Data'
+import { MainDiv, MentorsDiv, CircleDiv, SpinnerDiv, RoundDiv, GreenDiv, Image} from './StyledCmp'
+import {MainProfileDiv, ProfileImag, InfoDiv, RatingStarDiv, Span, SliderDiv, SliderImage, ArrowIcons } from './StyledCmp'
 
 function Mentors() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const MoveToPreviousSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    let newIndex = isFirstSlide ? MentorData.length-1 : currentIndex-1;
+    setCurrentIndex(newIndex);
+  }
+
+  const MoveToNextSlide = () => {
+    const isLastSlide = currentIndex === MentorData.length-1;
+    let newIndex = isLastSlide ? 0 : currentIndex+1;
+    setCurrentIndex(newIndex);
+  }
+
   return (
     <MainDiv>
         <MentorsDiv>
+        <SliderDiv>
+              <ArrowIcons onClick={MoveToPreviousSlide}><BsChevronLeft style={{width:'25px', height:'20px', marginTop:'9px'}}/></ArrowIcons>
+              <SliderImage src={MentorData[currentIndex].image}/>
+              <ArrowIcons onClick={MoveToNextSlide}><BsChevronRight style={{width:'25px', height:'20px', marginTop:'9px'}}/></ArrowIcons>
+            </SliderDiv>
+            <MainProfileDiv>
+              <RatingStarDiv>
+                <Span>{MentorData[currentIndex].rating}</Span>
+                <Span><BsFillStarFill/><BsFillStarFill/><BsFillStarFill/><BsFillStarFill/><BsFillStarFill/></Span>
+              </RatingStarDiv>
+            </MainProfileDiv>
             <CircleDiv>
-                <SpinnerDiv>
-                    <RoundDiv>1</RoundDiv>
-                    <RoundDiv>2</RoundDiv>
-                    <RoundDiv>3</RoundDiv>
-                    <RoundDiv>4</RoundDiv>
-                    <RoundDiv>5</RoundDiv>
-                </SpinnerDiv>
+              <GreenDiv>
+                    <SpinnerDiv>
+                    <><RoundDiv  style={{ transform: 'translate(251px)'}}><Image src={ImageUrls.firstUrl}/></RoundDiv>
+                    <RoundDiv style={{ transform: 'rotate(58deg) translate(251px) rotate(-58deg)' }}><Image src={ImageUrls.secondUrl}/></RoundDiv>
+                    <RoundDiv style={{ transform: 'rotate(125deg) translate(251px) rotate(-125deg)'}}><Image src={ImageUrls.thirdUrl}/></RoundDiv>
+                    <RoundDiv style={{ transform: 'rotate(182deg) translate(251px) rotate(-182deg)'}}><Image src={ImageUrls.fourthUrl}/></RoundDiv>
+                    <RoundDiv style={{ transform: 'rotate(270deg) translate(251px) rotate(-270deg)'}}><Image src={ImageUrls.fifthUrl}/></RoundDiv></>
+                    </SpinnerDiv>
+              </GreenDiv>
             </CircleDiv>
         </MentorsDiv>
     </MainDiv>
   )
 }
 
-export default Mentors
+export default Mentors;
+
--- src/Components/StyledCmp.js
+++ src/Components/StyledCmp.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 import {keyframes} from 'styled-components'
 
 export const rotate = keyframes`
-    100%{
+    30%{
         transform:rotate(1turn) translate(100px) rotate(-1turn)
     }
 `;
@@ -11,36 +11,79 @@ export const MainDiv = styled.div`padding:40px;`;
 
 export const MentorsDiv = styled.div`box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
 position:relative; width:100%; height:80vh; display:flex; jsutify-content:center; 
-align-items:center; overflow:hidden; 
+align-items:center; overflow:hidden;
+`;
+
+export const GreenDiv = styled.div`
+display:flex; jsutify-content:center; 
+align-items:center; 
 :before{
     content:' ';
     position:absolute;
     top:0;
     left:0;
-    width:80%;
+    width:1100px;
     height:80%;
     background:#C5F8C7;
     border-radius:0 0 50% 50%/0 0 100% 100%;
-    margin-left:215px;
+    margin-left:200px;
+ 
 }`;
 
 
 export const CircleDiv = styled.div`position:relative; z-index:1;
 margin:0 auto; max-width:900px; text-align:center;`;
 
-export const ContentDiv = styled.div``;
+export const Image = styled.img`width:120px; height:120px; border-radius:50%;`;
 
 export const P = styled.p`margin:0; padding:0; color:#fff; font-size:2em;`;
 
-export const SpinnerDiv = styled.div`--n:5; --d:12s; width:200px; height:200px; margin:40px auto; border:1px solid #000;
-display:grid; grid-template-columns:30px; grid-template-rows:30px; place-content:center; 
-border-radius:50%;`;
+export const SpinnerDiv = styled.div`--n:5; --d:12s;
+   width:501px;
+   height:501px;
+   border-radius:51%;
+   border:3px dashed rgba(0,0,0,0.5);
+   position:relative;
+   margin:200px;`;
 
-export const RoundDiv = styled.div`grid-area:1/1; line-height:30px; text-align: center; border-radius:50%; background:#f00; 
-animation: ${rotate} var(--d) linear infinite; transform: rotate(0) translate(100px) rotate(0);
-:nth-child(1) {animation-delay:calc(-0*var(--d)/var(--n))}
-:nth-child(2) {animation-delay:calc(-1*var(--d)/var(--n))}
-:nth-child(3) {animation-delay:calc(-2*var(--d)/var(--n))}
-:nth-child(4) {animation-delay:calc(-3*var(--d)/var(--n))}
-:nth-child(5) {animation-delay:calc(-4*var(--d)/var(--n))}`;
+   export const RoundDiv = styled.div`
+   display:block;
+   position:absolute;
+   top:51%;
+   left:51%;
+   width:100px;
+   height:100px;
+   margin:-51px; 
+   border-radius:51%;
+   text-align:center;
+   line-height:100px;
+   padding:20px;
+   animation: ${rotate} var(--d) linear infinite;`;
 
+// Slider
+
+export const MainProfileDiv = styled.div``;
+
+export const InfoDiv = styled.div``;
+
+export const RatingStarDiv = styled.div``;
+
+export const Span = styled.span`display:flex;`;
+export const MentorName = styled.h3``;
+
+
+export const SliderDiv = styled.div`display:flex; margin-bottom:330px; padding:20px;`;
+export const SliderImage = styled.img`width:170px; height:170px; border-radius:50%; margin:30px;`;
+export const ArrowIcons = styled.div`margin-top:10px; background:#B2B2B2; padding:8px;
+cursor:pointer; height:40px; width:40px; text-align:center; border-radius:50%;`;
+
+
+// Style image objects
+export const ImageUrls = {
+    firstUrl:'https://firebasestorage.googleapis.com/v0/b/mentro-app.appspot.com/o/users%2FuieMBX2wmaPXBiJdGFR2aA3RHFo1%2Fprofile.jpg?alt=media&token=08d279f4-ba61-4e21-ad33-237240f16177)',
+    secondUrl :'https://firebasestorage.googleapis.com/v0/b/mentro-app.appspot.com/o/users%2FSVn8t0viushTRU125C8COvlRhms2%2Fprofile.jpg?alt=media&token=d20a3ad2-08eb-4abb-8fa1-c90c9358d783',
+    thirdUrl : 'https://firebasestorage.googleapis.com/v0/b/mentro-app.appspot.com/o/users%2FyR5wjuAxScYl82BMDsFU4SKBVGw2%2Fprofile.jpg?alt=media&token=4a1375e8-edf8-46c1-94ff-9f444dce654e',
+    fourthUrl:'https://firebasestorage.googleapis.com/v0/b/mentro-app.appspot.com/o/users%2FxvDFQPqb8iMjilqE8lCQaVtFVWR2%2Fprofile.jpg?alt=media&token=9dc083c2-7733-4012-860f-aeaa0c99fd48',
+    fifthUrl:'https://firebasestorage.googleapis.com/v0/b/mentro-app.appspot.com/o/users%2FxwXGq3VWeXQhEE4LsfguPpTjpom2%2Fprofile.jpg?alt=media&token=a097b407-89f9-468e-a2d0-9b49523bf150'
+
+}
